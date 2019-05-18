fbc51ff8bda5fd10f7a5751a5972919f59f474ef
condini-mastheus <matheus.condini@gmail.com>
2019-05-18 15:53:36 -0300

    Configurate main screen'

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { Container } from './styles';
 
-const Main = () => <View />;
-
-export default Main;
+export default function Main() {
+  return <Container />;
+}
